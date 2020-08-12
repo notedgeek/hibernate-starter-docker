569d5d7954210ae72ff19ab48cc49352370a1627
seye <notedgeek@gmail.com>
2020-08-12 11:27:21 +0100

    fix

--- init-development.sql
+++ init-development.sql
@@ -1,13 +1,11 @@
 CREATE database test;
 
 CREATE USER 'test'@'%' IDENTIFIED BY 'test';
-CREATE USER 'test'@'localhost' IDENTIFIED BY 'test';
 
 GRANT ALL ON test.* TO 'test'@'%';
-GRANT ALL ON test.* TO 'test'@'localhost';
 
 use test;
 
 create table simon(a integer);
-insert into simon values(48);
+insert into simon values(51);
 commit;
